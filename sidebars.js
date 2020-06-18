b6cdd5a4f632e04ff67ef518d954e3df55cda935
Oscar Pineda <friedchicken@Oscars-MacBook-Pro.local>
2020-06-17 21:08:22 -0500

    relaxation

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   someSidebar: {
     "Security Plus": ['study-guide', 'udemy-comptia'],
+    "Productivity" : ["relaxing"],
     Docusaurus: ['doc1', 'doc2', 'doc3'],
     Features: ['mdx'],
   },
